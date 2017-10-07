3dc6aff71a5c9b48bc89bf6e85ff591bd063b3a5
Gary Roberts <tallstreet@gmail.com>
2017-10-07 20:18:57 +0100

    Display logs

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    fetch("/logs")
+      .then(res => res.json())
+      .then(resp => {
+        this.setState({ resp });
+      });
+  }
+
+  renderRow(row) {
+    return (
+      <tr>
+        <td>{row[0]}</td>
+        <td>{row[1]}</td>
+        <td>{row[2]}</td>
+        <td>{row[3]}</td>
+        <td>{row[4]}</td>
+        <td>{row[5]}</td>
+      </tr>
+    );
+  }
+
+  renderTable() {
+    return (
+      <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Device</th>
+            <th>Time</th>
+            <th>Date</th>
+            <th>Location</th>
+            <th>Label</th>
+          </tr>
+        </thead>
+        <tbody>{this.state.resp.map(this.renderRow)}</tbody>
+      </table>
+    );
+  }
 
   render() {
-    return (
-      <div className="">
-        Home
-      </div>
-    );
+    return <div className=""> {this.state.resp && this.renderTable()}</div>;
   }
 }
