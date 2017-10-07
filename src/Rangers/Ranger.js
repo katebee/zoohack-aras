125cdafecdac90b3aa076c668d75db2f83ec4a2d
Kate Whalen <whalenka88@gmail.com>
2017-10-07 23:14:53 +0100

    Add cards with ranger details and styling for Rangers view

---
+++ src/Rangers/Ranger.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './Ranger.css';
+
+export default class Ranger extends Component {
+  render() {
+    return (
+      <div className="mdl-card mdl-shadow--2dp ranger-card">
+        <div className="mdl-card__title">
+          <i className="material-icons mdl-list__item-avatar">person</i>
+          <h2 className="mdl-card__title-text ranger-name">{this.props.data.name}</h2>
+        </div>
+        <div className="mdl-card__supporting-text ranger-details">
+          <i className="material-icons">location_on</i>
+          <span className="ranger-details__item">
+            {' '}Location:{' '}{this.props.data.lat}{', '}{this.props.data.lon}
+          </span>
+        </div>
+        <div className="mdl-card__supporting-text ranger-details">
+          <i className="material-icons">phone</i>
+          <span className="ranger-details__item">
+            {' '}Number:{' '}{this.props.data.tel}
+          </span>
+        </div>
+        <div className="mdl-card__actions mdl-card--border">
+          <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"><i class="material-icons">message</i>
+            {' '}Message
+          </a>
+        </div>
+        <div className="mdl-card__menu">
+          <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+            <i className="material-icons">mode_edit</i>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
--- src/Rangers/Rangers.js
+++ src/Rangers/Rangers.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import Ranger from './Ranger'
 
 export default class Rangers extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rangers: [
+        {
+          lat: -2.48,
+          lon: 38.18,
+          name: 'Kate Whalen',
+          tel: '+4412341234'
+        },
+        {
+          lat: -2.48,
+          lon: 38.18,
+          name: 'Guy Fawkes',
+          tel: '+4405111605'
+        },
+        {
+          lat: -2.48,
+          lon: 38.18,
+          name: 'Terry Pratchett',
+          tel: '+4412341234'
+        },
+      ]
+
+    };
+  }
 
   render() {
     return (
-      <div className="">
-        Rangers
+      <div>
+        {this.state.rangers.map((ranger, i) => (
+          <Ranger data={ranger} key={i} />
+        ))}
+
+        <div className="mdl-card mdl-shadow--2dp ranger-add">
+          <div className="mdl-card__title">
+            <h2 className="mdl-card__title-text ranger-add__text">Add new</h2>
+            <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect ranger-add__button">
+              <i className="material-icons">add</i>
+            </button>
+          </div>
+      </div>
+
       </div>
     );
   }
